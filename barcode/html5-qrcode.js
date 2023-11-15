1395a64cac255b7a7d69a7e86d4ef259d337cf58
kimddingjae <93251858+kimddingjae@users.noreply.github.com>
2023-11-15 16:03:38 +0900

    Update html5-qrcode.js

--- barcode/html5-qrcode.js
+++ barcode/html5-qrcode.js
@@ -1,4 +1,4 @@
-
+/*
 function onScanSuccess(decodedText, decodedResult) {
     // Handle on success condition with the decoded text or result.
     alert(decodedText)
@@ -17,16 +17,14 @@ const scanner = new Html5QrcodeScanner('reader', {
             },
             false)
 scanner.render(onScanSuccess);
-
-/*
-const html5QrCode = new Html5Qrcode("reader");
-
-// if you scanned , it will be write in clear text in your input field which in my case 'result'
-const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-    alert(decodedText)
-};
-const config = { fps: 200, qrbox: 400 };
-
-// prefer the back camera else the front one 
-html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
 */
+
+const html5QrCode = new Html5Qrcode("reader");
+const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    alert(decodedText);
+};
+const config = { fps: 10, qrbox: { width: 250, height: 250 } };
+
+// If you want to prefer front camera
+html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+
