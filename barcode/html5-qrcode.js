79cce430efff9a465720bb944e3db0e4c3cda9ee
kimddingjae <93251858+kimddingjae@users.noreply.github.com>
2023-11-15 16:53:39 +0900

    Update html5-qrcode.js

--- barcode/html5-qrcode.js
+++ barcode/html5-qrcode.js
@@ -1,24 +1,18 @@
-/*
-function onScanSuccess(decodedText, decodedResult) {
-    // Handle on success condition with the decoded text or result.
-    alert(decodedText)
-    console.log(`Scan result: ${decodedText}`, decodedResult);
-    // ...
-    //html5QrcodeScanner.clear();
-    // ^ this will stop the scanner (video feed) and clear the scan area.
-}
-const scanner = new Html5QrcodeScanner('reader', {
-                qrbox: {
-                    width: 200,
-                    height: 200,
-                },
-                fps: 5,
-               videoConstraints: { facingMode: { exact: "environment" } },
-            },
-            false)
-scanner.render(onScanSuccess);
-*/
 
+const html5QrCode = new Html5Qrcode("reader");
+
+// if you scanned , it will be write in clear text in your input field which in my case 'result'
+const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+    alert(decodedText)
+    //document.getElementById('result').value = decodedText;
+};
+const config = { fps: 5, qrbox: 200 };
+
+// prefer the back camera else the front one 
+html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
+
+
+/*
 function onScanSuccess(decodedText, decodedResult) {
   // handle the scanned code as you like, for example:
   alert(decodedText);
@@ -38,5 +32,5 @@ html5QrcodeScanner.render(onScanSuccess);
 
 //const start = document.getElementById('html5-qrcode-button-camera-permission');
 //start.click();
-
+*/
 
