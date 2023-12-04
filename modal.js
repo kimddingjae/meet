be1aaf1a9f392ca5a66917dd18d000b1a381b9f0
kimddingjae <93251858+kimddingjae@users.noreply.github.com>
2023-12-04 14:44:50 +0900

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,6 +1,15 @@
 const body = document.querySelector("body");
 const modal = document.querySelector(".modal");
+const x = document.querySelector(".x");
 const btnOpenPopup = document.querySelector(".btn-open-popup");
+
+x.addEventListener("click", () => {
+  modal.classList.toggle("show");
+
+  if (modal.classList.contains("show")) {
+    body.style.overflow = "hidden";
+  }
+});
 
 btnOpenPopup.addEventListener("click", () => {
   modal.classList.toggle("show");
