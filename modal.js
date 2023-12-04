3048253e76dca3a4738219b60065001c3937238c
kimddingjae <93251858+kimddingjae@users.noreply.github.com>
2023-12-04 15:12:55 +0900

    Update modal.js

--- modal.js
+++ modal.js
@@ -2,26 +2,12 @@ const body = document.querySelector("body");
 const modal = document.querySelector(".modal");
 const btnOpenPopup = document.querySelector(".btn-open-popup");
 
-function close(){
-  alert("닫기");
-  modal.classList.toggle("show");
-  body.style.overflow = "auto";
-}
-
 btnOpenPopup.addEventListener("click", () => {
   modal.classList.toggle("show");
-
-  if (modal.classList.contains("show")) {
-    body.style.overflow = "hidden";
-  }
 });
 
 modal.addEventListener("click", (event) => {
   if (event.target === modal) {
     modal.classList.toggle("show");
-
-    if (!modal.classList.contains("show")) {
-      body.style.overflow = "auto";
-    }
   }
 });
