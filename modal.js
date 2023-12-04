02dad23cd482d98771342b1fe0df5b856a4e355e
kimddingjae <93251858+kimddingjae@users.noreply.github.com>
2023-12-04 16:07:38 +0900

    Update modal.js

--- modal.js
+++ modal.js
@@ -5,7 +5,7 @@ const btnOpenPopup = document.querySelector(".btn-open-popup");
  window.addEventListener('message', handleDocHeightMsg, false); // 메시지 수신 이벤트 등록
 
 function handleDocHeightMsg(eventObj) { // 메시지 수신 처리를 위한 함수
-    console.log(eventObj.data);
+    modal.classList.toggle("show");
 }
 
 btnOpenPopup.addEventListener("click", () => {
